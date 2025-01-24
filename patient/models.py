ba4e9da0537ca4da545572f362cc23f9ed1c1c95
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-01-24 21:41:40 +0600

    создание моделей

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    is_doctor = models.BooleanField
+
+
+class Patient(models.Model):
+    id = models.IntegerField(primary_key=True, db_index=True)
+    date_of_birth = models.DateField()
+
+
+class Diagnose(models.Model):
+    id = models.IntegerField(primary_key=True, db_index=True)
+    name = models.CharField(max_length=125, )
+    description = models.TextField()
+    patient = models.ForeignKey(Patient, related_name="diagnoses")

94fa7aab24e69bed2e86add7143d3f76fd2a7a52
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-01-25 01:10:12 +0600

    настройка админки

--- patient/admin.py
+++ patient/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Patient, Diagnose
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ["is_doctor"]}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ["is_doctor"]}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Patient)
+admin.site.register(Diagnose)

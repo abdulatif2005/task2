e5f15f932776a7b6028d4c31ddbd52ed17c5b7e7
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-01-24 22:23:20 +0600

    добавлены новые параметры вsettings.py

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'patient.apps.PatientConfig'
+    'patient.apps.PatientConfig',
+    'rest_framework_simplejwt',
 
 ]
 
@@ -133,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "patient.CustomUser"

b6a5fa6b132ebe51873514bacb40c67691593468
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-01-25 01:17:48 +0600

    добавлен endpoint для токена

--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Patient
+from .serializers import PatientSerializer, CustomTokenObtainPairSerializer
+from .permissions import IsDoctorPermission
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class PatientListAPIView(generics.ListAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+    permission_classes = [IsDoctorPermission]
+
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer

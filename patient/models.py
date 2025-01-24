c945002f0d7e33524a109a53aba5c22193cfd607
Абдулатиф Имаров <abdulatifimarov@gmail.com>
2025-01-25 01:13:48 +0600

    исправлено ошибка с полями id моделей

--- patient/models.py
+++ patient/models.py
@@ -3,17 +3,15 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    is_doctor = models.BooleanField
+    is_doctor = models.BooleanField(default=False)
 
 
 class Patient(models.Model):
-    id = models.IntegerField(primary_key=True, db_index=True)
     date_of_birth = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Diagnose(models.Model):
-    id = models.IntegerField(primary_key=True, db_index=True)
     name = models.CharField(max_length=125, )
     description = models.TextField()
-    patient = models.ForeignKey(Patient, related_name="diagnoses")
+    patient = models.ForeignKey(Patient, related_name="diagnoses", on_delete=models.CASCADE)
